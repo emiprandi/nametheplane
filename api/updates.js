93d486274cd5bd034be815efb940bde22c8065cf
Emiliano Prandi <emiprandi@gmail.com>
2021-02-15 01:31:54 +0100

    query test

--- api/updates.js
+++ api/updates.js
@@ -1,10 +1,15 @@
 const got = require('got');
 
 module.exports = (req, res) => {
-  res.json({
-    body: req.body,
-    query: req.query,
-    cookies: req.cookies,
-  })
+
+  got.post('https://webhook.site/9e6e3d2f-3614-4ba0-90e2-1f320192f561', {
+    json: {
+      body: req.body,
+      query: req.query,
+      cookies: req.cookies,
+    }
+  });
+
+  res.json(true)
 }
 
