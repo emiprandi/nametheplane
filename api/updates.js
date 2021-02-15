1bab8caf562261f517ac68a0dee2ed7b726bc4b7
Emiliano Prandi <emiprandi@gmail.com>
2021-02-15 01:40:51 +0100

    more testing

--- api/updates.js
+++ api/updates.js
@@ -1,15 +1,25 @@
 const got = require('got');
 
 module.exports = (req, res) => {
+  let result
 
-  got.post('https://webhook.site/9e6e3d2f-3614-4ba0-90e2-1f320192f561', {
-    json: {
-      body: req.body,
-      query: req.query,
-      cookies: req.cookies,
+  (async () => {
+    try {
+      const {body} = await got.post('https://webhook.site/9e6e3d2f-3614-4ba0-90e2-1f320192f561', {
+        json: {
+          body: req.body,
+          query: req.query,
+          cookies: req.cookies,
+        },
+        responseType: 'json'
+      });
+
+      result = body
+    } catch (err) {
+      result = 'Somthing went really bad :' + err
     }
-  });
+  })();
 
-  res.json(true)
+  res.send(result)
 }
 
