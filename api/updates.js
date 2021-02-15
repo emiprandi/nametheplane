4c9abe7e0e10656f56f0cfe64cf21e24d4cecb47
Emiliano Prandi <emiprandi@gmail.com>
2021-02-15 09:40:07 +0100

    resonse send test

--- api/updates.js
+++ api/updates.js
@@ -1,14 +1,14 @@
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
   const msg = req.body
   console.log(msg.message)
 
-  // await got.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
-  //   retry: 0,
-  //   json: {
-  //     chat_id: msg.chat.id,
-  //     text: 'Si, soy yo!!!',
-  //   },
-  // });
+  await got.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
+    retry: 0,
+    json: {
+      chat_id: msg.chat.id,
+      text: 'Si, soy yo!!!',
+    },
+  });
 
-  res.status(200)
+  res.send('ok')
 }
