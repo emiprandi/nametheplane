5f02426656a8074b672c0b626ca6d5b8f465b562
Emiliano Prandi <emiprandi@gmail.com>
2021-02-15 02:38:58 +0100

    disable req

--- api/updates.js
+++ api/updates.js
@@ -1,15 +1,14 @@
-const got = require('got');
-
 module.exports = async (req, res) => {
   const msg = req.body.message
   console.log(msg)
 
-  await got.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
-    json: {
-      chat_id: msg.chat.id,
-      text: 'Si, soy yo!!!',
-    },
-  });
+  // await got.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
+  //   retry: 0,
+  //   json: {
+  //     chat_id: msg.chat.id,
+  //     text: 'Si, soy yo!!!',
+  //   },
+  // });
 
   res.status(200)
 }
