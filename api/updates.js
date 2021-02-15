438fc169e714c2d07d4e6ec9971e91b289df241e
Emiliano Prandi <emiprandi@gmail.com>
2021-02-15 09:50:37 +0100

    key fix

--- api/updates.js
+++ api/updates.js
@@ -7,7 +7,7 @@ module.exports = async (req, res) => {
   await got.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
     retry: 0,
     json: {
-      chat_id: msg.chat.id,
+      chat_id: msg.message.chat.id,
       text: 'Si, soy yo!!!',
     },
   });
