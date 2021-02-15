32bc88df9f63c4e0c976f0fb49bf4daf118c605a
Emiliano Prandi <emiprandi@gmail.com>
2021-02-15 02:35:21 +0100

    await call

--- api/updates.js
+++ api/updates.js
@@ -1,10 +1,10 @@
 const got = require('got');
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
   const msg = req.body.message
   console.log(msg)
 
-  got.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
+  await got.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
     json: {
       chat_id: msg.chat.id,
       text: 'Si, soy yo!!!',
