e27d3b946cd78df55efbcea2c0b29acbe0c08fb7
Emiliano Prandi <emiprandi@gmail.com>
2021-02-19 09:57:35 +0100

    debug

--- services/telegram.js
+++ services/telegram.js
@@ -2,6 +2,7 @@ const got = require('got');
 
 const sendMessage = async (chat, message) => {
   try {
+    console.log('sending msg to:', chat, process.env.BOT_TOKEN)
     await got.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
       retry: 0,
       json: {
