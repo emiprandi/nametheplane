aa5a9a408b2f801163362f616a52df2c0138ba81
Emiliano Prandi <emiprandi@gmail.com>
2021-02-19 10:09:44 +0100

    no aircrafts message

--- api/updates.js
+++ api/updates.js
@@ -10,11 +10,15 @@ module.exports = async (req, res) => {
     const boundBox = getGeoArea(msg.message.location)
     const planes = await getAircraftsInABox(boundBox)
 
-    planes.forEach(async plane => {
-      await sendMessage(msg.message.chat.id, `${plane.aircraft.manufacturer} ${plane.aircraft.model}
-      Operated by: ${plane.aircraft.operator} (${plane.aircraft.country})
-      Route: ${plane.route.join(' -> ')}`)
-    })
+    if (planes.length > 0) {
+      planes.forEach(async plane => {
+        await sendMessage(msg.message.chat.id, `${plane.aircraft.manufacturer} ${plane.aircraft.model}
+        Operated by: ${plane.aircraft.operator} (${plane.aircraft.country})
+        Route: ${plane.route.join(' -> ')}`)
+      })
+    } else {
+      await sendMessage(msg.message.chat.id, 'I didn\'t find any aircrafts in this area')
+    }
   } else {
     // we received a message, but we don't support this type yet, so let's reply politely
     await sendMessage(msg.message.chat.id, 'Hi there! I only understand location messages so far. Please send me a location and I will tell you if there are planes flighing in that area.')
