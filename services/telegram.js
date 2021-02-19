2687ec16229fc8cc334115288b29f62b7ce03ef2
Emiliano Prandi <emiprandi@gmail.com>
2021-02-19 10:00:44 +0100

    log response

--- services/telegram.js
+++ services/telegram.js
@@ -2,8 +2,7 @@ const got = require('got');
 
 const sendMessage = async (chat, message) => {
   try {
-    console.log('sending msg to:', chat, process.env.BOT_TOKEN)
-    await got.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
+    const response = await got.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
       retry: 0,
       json: {
         chat_id: chat,
@@ -11,6 +10,7 @@ const sendMessage = async (chat, message) => {
         text: message,
       },
     })
+    console.log(response.body)
   } catch (err) {
   }
 }
